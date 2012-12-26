97a61be2c262eff56ecfd48983d8d943849337b8
Phil Kallos <phil.kallos@gmail.com>
2012-12-25 19:36:37 -0800

    God damn I am a hero. So much progress.
    * Added support for profiling CPU
    * Disable HTTP GZIP compression (performance)
    * Log timings
    * Log averages
    * Create structs instead of using hashes
    * Handle SIGTERM

--- stampede.go
+++ stampede.go
@@ -4,17 +4,37 @@ import (
     "flag"
     "fmt"
     "os"
+    "os/signal"
     "bufio"
     "bytes"
+    "log"
     "io"
     "net/http"
     "math/rand"
     "time"
     "sync"
+    "runtime/pprof"
 )
+
+type Response struct {
+    StatusCode      int
+    Time            time.Duration
+}
+
+type ResponseStats struct {
+    Status2xx       int
+    Status3xx       int
+    Status4xx       int
+    Status5xx       int
+    Total           int
+    TotalTime       float64
+    MaxTime         int
+    MinTime         int
+}
 
 var (
     input_file       = flag.String  ("f", "", "Input file to read from.")
+    profile          = flag.String  ("p", "", "Write out CPU profile.")
     clients          = flag.Int     ("c", 10, "The number of buffalo to spawn.")
     duration         = flag.Int     ("t", 30, "The duration of the stampede in seconds.")
     wait_duration    = flag.Int     ("w", 1, "The time clients wait between HTPT requests (in seconds).")
@@ -26,18 +46,22 @@ var (
     // This structure is not threadsafe AFAIK so
     // it can only be modifed and read from a single
     // goroutine.
-    codestats = map[string] int {
-        "2xx":      0,
-        "3xx":      0,
-        "4xx":      0,
-        "5xx":      0,
-        "success":  0,
-        "fail":     0,
+    response_stats = ResponseStats {
+        Status2xx:   0,
+        Status3xx:   0,
+        Status4xx:   0,
+        Status5xx:   0,
+        Total:       0,
+        TotalTime:   0,
+        MaxTime:     0,
+        MinTime:     2048000000,
     }
 
     // Some waitgroups, baby!
     producers_wg sync.WaitGroup
     consumers_wg sync.WaitGroup
+
+    os_signals chan os.Signal
 )
 
 func usage() {
@@ -48,12 +72,8 @@ func usage() {
 }
 
 func begin_stampede(endpoints []string) {
-    codes       := make(chan int)
+    responses   := make(chan Response, *clients * 100)
     timeout     := make(chan bool)
-    transport   := &http.Transport {
-        DisableKeepAlives: false,
-    }
-    client      := &http.Client { Transport: transport }
 
     // After timeout, fire done signals
     go func() {
@@ -62,6 +82,9 @@ func begin_stampede(endpoints []string) {
         for i := 0; i < *clients; i++ { timeout <- true }
     }()
 
+    // Record the start time
+    start_time := time.Now()
+
     // Create the clients (producers)
     for i := 0; i < *clients; i++ {
         producers_wg.Add(1)
@@ -69,6 +92,11 @@ func begin_stampede(endpoints []string) {
             var url string
             max     := len(endpoints)
             counter := 0
+            transport   := &http.Transport {
+                DisableKeepAlives  : false,
+                DisableCompression : false,
+            }
+            client      := &http.Client { Transport: transport }
 
 // This feels hyper naughty but I swear I read somewhere to do this
 // to break out of a for-select
@@ -95,19 +123,21 @@ sadtimeslabel:
                             fmt.Printf("Requesting url %s\n", url)
                         }
 
+                        start := time.Now()
                         resp, err := client.Get(url)
+                        elapsed := time.Since(start)
+                        defer resp.Body.Close()
 
                         if err != nil {
                             fmt.Fprintf(os.Stderr, "Failed to connect to %s, %s\n", url, err)
-                            codes <- 500
+                            responses <- Response { StatusCode: 500, Time: 10 * time.Second }
                         } else {
-                            codes <- resp.StatusCode
+                            responses <- Response { StatusCode: resp.StatusCode, Time: elapsed }
                         }
 
                         if (*wait_duration >= 0) {
                             time.Sleep(time.Duration(*wait_duration) * time.Second)
                         }
-                        defer resp.Body.Close()
                 }
             }
             producers_wg.Done()
@@ -117,41 +147,60 @@ sadtimeslabel:
     // Build the consumer that aggregates the stats
     consumers_wg.Add(1)
     go func () {
-        for code := range(codes) {
-            if (*verbose) {
-                fmt.Printf("Received code %d\n", code)
-            }
+        for response := range(responses) {
+            select {
+                case <- os_signals:
+                    fmt.Printf("Caught signal, quitting\n...")
+                    for i := 0; i < *clients; i++ { timeout <- true }
+                default:
+                    if *verbose {
+                        fmt.Printf("Received code %d time %d\n", response.StatusCode, response.Time)
+                    }
 
-            if (code >= 200 && code < 300) {
-                codestats["2xx"] = codestats["2xx"] + 1
-                codestats["success"] = codestats["success"] + 1
-            } else if (code >= 300 && code < 400) {
-                codestats["3xx"] = codestats["3xx"] + 1
-                codestats["success"] = codestats["success"] + 1
-            } else if (code >= 400 && code < 500) {
-                codestats["4xx"] = codestats["4xx"] + 1
-                codestats["success"] = codestats["success"] + 1
-            } else if (code >= 500 && code < 600) {
-                codestats["5xx"] = codestats["5xx"] + 1
-                codestats["fail"] = codestats["fail"] + 1
+                    response_stats.Total++
+                    response_stats.TotalTime += float64(response.Time)
+
+                    if int(response.Time) > response_stats.MaxTime {
+                        response_stats.MaxTime = int(response.Time)
+                    }
+                    if int(response.Time) < response_stats.MinTime {
+                        response_stats.MinTime = int(response.Time)
+                    }
+
+                    if (response.StatusCode >= 200 && response.StatusCode < 300) {
+                        response_stats.Status2xx++
+                    } else if (response.StatusCode >= 300 && response.StatusCode < 400) {
+                        response_stats.Status3xx++
+                    } else if (response.StatusCode >= 400 && response.StatusCode < 500) {
+                        response_stats.Status4xx++
+                    } else if (response.StatusCode >= 500 && response.StatusCode < 600) {
+                        response_stats.Status5xx++
+                    }
             }
         }
         consumers_wg.Done()
     }()
 
     producers_wg.Wait()
-    close(codes)
+    close(responses)
     consumers_wg.Wait()
+
+    // Record the elapsed time
+    elapsed_time := time.Since(start_time)
 
     // Print out the response code histogram
     fmt.Printf("\nReponse codes:\n")
-    fmt.Printf("[%s]:\t%d\n", "2xx", codestats["2xx"])
-    fmt.Printf("[%s]:\t%d\n", "3xx", codestats["3xx"])
-    fmt.Printf("[%s]:\t%d\n", "4xx", codestats["4xx"])
-    fmt.Printf("[%s]:\t%d\n", "5xx", codestats["5xx"])
+    fmt.Printf("[%s]:\t%d\n", "2xx", response_stats.Status2xx)
+    fmt.Printf("[%s]:\t%d\n", "3xx", response_stats.Status3xx)
+    fmt.Printf("[%s]:\t%d\n", "4xx", response_stats.Status4xx)
+    fmt.Printf("[%s]:\t%d\n", "5xx", response_stats.Status5xx)
 
-    fmt.Printf("\nTotal Requests:\t%d\n", codestats["success"] + codestats["fail"])
-    fmt.Printf("Availability:\t%4.2f\n", float32(codestats["success"]) * 100.0 / float32(codestats["fail"] + codestats["success"]))
+    fmt.Printf("\nTotal Requests:\t%d\n", response_stats.Total)
+    fmt.Printf("Max time:\t%4.2fms\n", float64(response_stats.MaxTime) / float64(time.Millisecond))
+    fmt.Printf("Min time:\t%4.2fms\n", float64(response_stats.MinTime) / float64(time.Millisecond))
+    fmt.Printf("Avg time:\t%4.2fms\n", float64(response_stats.TotalTime) / float64(response_stats.Total) / float64(time.Millisecond))
+    fmt.Printf("Total time:\t%4.2fs\n", float64(elapsed_time) / float64(time.Second))
+    fmt.Printf("Avg Throughput:\t%4.2f req/s\n", float64(response_stats.Total) * float64(time.Second) / float64(elapsed_time))
 }
 
 // Read a whole file into the memory and store it as array of lines
@@ -187,15 +236,27 @@ func read_lines(path string) (lines []string, err error) {
 func init() {
     // Seed the RNG
     rand.Seed(time.Now().UTC().UnixNano())
-}
-
-func main() {
-    var endpoints []string
 
     // Set up command line flags
     flag.Usage = usage
     flag.Parse()
+
+    os_signals = make(chan os.Signal, 1)
+    signal.Notify(os_signals)
+}
+
+func main() {
+    var endpoints []string
     args := flag.Args()
+
+    if *profile != "" {
+        f, err := os.Create(*profile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
 
     if *show_help {
         flag.Usage()
